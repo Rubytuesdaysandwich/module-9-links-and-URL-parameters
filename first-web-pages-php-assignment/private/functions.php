cc7c5c4c40e36795cc1d5009460ecbb3e9e4c661
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-06-09 14:25:52 -0600

    finction get page id

---
+++ first-web-pages-php-assignment/private/functions.php
@@ -0,0 +1,14 @@
+<?php
+function get_id(){
+
+ $page =$_GET['page'];
+ return $page;
+}
+
+function get_title(){
+    $title
+}
+
+
+
+?>
---
+++ first-web-pages-php-assignment/private/shared/links_header/links_header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+    <header>
+        <ul style="list-style: none;">
+            <li><a href="links2.php?page=1">Page 1</a></li><!--path to page1-->
+            <li><a href="links2.php?page=2">Page 2</a></li><!--path to page2-->
+            <li><a href="links2.php?page=3">Page 3</a></li><!--path to page3-->
+            <li><a href="links2.php?page=4">Page 4</a></li><!--path to page4-->
+            <li><a href="links2.php?page=5">Page 5</a></li><!--path to page5-->
+        </ul>
+    </header>
