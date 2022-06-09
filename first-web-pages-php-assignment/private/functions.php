5b7e68b19d8331202388b4d5259b6110ed61751c
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-06-09 16:17:00 -0600

    update switch

--- first-web-pages-php-assignment/private/functions.php
+++ first-web-pages-php-assignment/private/functions.php
@@ -2,13 +2,32 @@
 function get_id(){
 
  $page =$_GET['page']??'1';// php 7.0 ?? gives it a default if nothing is given
- return $page;
+ return $page;//sending the variable outside the function get_id can now be accessed outside of function
 }
 
 function get_title(){
- $page = get_id();
-   $title= $_GET['title']??'home';// php 7.0 gives it a default if nothing is given
-   return $title;
+ $page = get_id();//pulling in get_id function and the $page variable to get the page id
+    switch($page){
+        case 0:
+        echo "i equals 0";
+        break;
+        case 1:
+        echo "i equals 0";
+        break;
+        case 2:
+        echo "i equals 0";
+        break;
+        case 3:
+        echo "i equals 0";
+        break;
+        case 4:
+        echo "i equals 0";
+        break;
+        case 5:
+        echo "i equals 0";
+        break;                                  
+        
+    }
 }
 
 
